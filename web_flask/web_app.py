708596bc4f8fdcb2e6dc3df26b98c76c15939410
DayC <kongranalanyuy@gmail.com>
2024-07-11 13:20:35 +0100

    web_app in progress

--- web_flask/web_app.py
+++ web_flask/web_app.py
@@ -5,8 +5,11 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/", strict_slaches=False)
+@app.route("/")
 def index():
     """Home"""
 
-    return render_template("index.html")
+    return render_template("search_drug.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
